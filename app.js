b36f94b4e1962905a243f2f476d5c898891f7e0b
Guilherme Ferreira <gui_henrique09@hotmail.com>
2024-10-15 19:31:35 -0300

    Implementação de login

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const { engine } = require('express-handlebars');
 const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
-
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const User = require('./dao/models/user');
+const Product = require('./dao/models/product'); 
 const port = 8080;
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'seuSegredo',
+    resave: false,
+    saveUninitialized: true
+}));
 
 // Servir o arquivo socket.io.js
 app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/client-dist'));
@@ -32,7 +41,46 @@ app.get('/realtimeproducts', (req, res) => {
     res.render('realTimeProducts', { products: productsData });
 });
 
+// Rota para registro
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userRole = email === 'adminCoder@coder.com' ? 'admin' : 'user';
+    const user = new User({ email, password: hashedPassword, role: userRole });
+    await user.save();
+    res.redirect('/login');
+});
+
+// Rota para login
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && await bcrypt.compare(password, user.password)) {
+        req.session.user = user;
+        res.redirect('/products');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Rota para logout
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+});
+
 app.get('/products', async (req, res) => {
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
     try {
         const { limit = 10, page = 1, sort, query } = req.query;
         const options = {
@@ -40,11 +88,10 @@ app.get('/products', async (req, res) => {
             limit: parseInt(limit, 10),
             sort: sort === 'asc' ? { price: 1 } : sort === 'desc' ? { price: -1 } : {},
         };
-
         const filter = query ? { title: new RegExp(query, 'i') } : {};
-
         const result = await Product.paginate(filter, options);
         res.render('products', {
+            user: req.session.user,
             products: result.docs,
             totalPages: result.totalPages,
             prevPage: result.prevPage,
@@ -58,10 +105,6 @@ app.get('/products', async (req, res) => {
     } catch (error) {
         res.status(500).json({ status: 'erro', error: error.message });
     }
-});
-
-app.get('/chat', (req, res) => {
-    res.render('chat');
 });
 
 // Conectar ao MongoDB
@@ -83,13 +126,11 @@ const Message = require('./dao/models/message');
 // Servidor de chat
 io.on('connection', (socket) => {
     console.log('Um usuário conectou');
-    
     socket.on('sendMessage', async (data) => {
         const newMessage = new Message(data);
         await newMessage.save();
         io.emit('newMessage', data);
     });
-
     socket.on('disconnect', () => {
         console.log('Um usuário desconectou');
     });
